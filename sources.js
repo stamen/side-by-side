2ec10f64b542b51f1b10bdc8ef12d40b2ecb3b62
Alan McConchie <alan.mcconchie@gmail.com>
2015-10-22 17:31:12 -0700

    better names

--- sources.js
+++ sources.js
@@ -1,9 +1,9 @@
-var default_A = 'Stamen Terrain';
-var default_B = 'Terrain Classic (local)';
+var default_A = 'Stamen Terrain (deployed)';
+var default_B = 'Terrain Classic (local dev)';
 
 var sources = {
   localterrain: {
-    name: "Terrain Classic (local)",
+    name: "Terrain Classic (local dev)",
     url: "http://localhost:8080/{z}/{x}/{y}.png"
   },
   google: {
@@ -19,7 +19,7 @@ var sources = {
     url: "http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png"
   },
   terrain: {
-    name: "Stamen Terrain",
+    name: "Stamen Terrain (deployed)",
     url: "http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.png"
   },
   positron: {
